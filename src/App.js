afcd762e7a1cf43eacd49f6482b0e96eda2bc6b0
Pravesh kumar bind <108334271+Pravesh-MW@users.noreply.github.com>
2024-08-27 08:38:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const API_BASE_URL = "https://api-s27s.onrender.com"
 function App() {
   const [presentation, setPresentation] = useState('TS');
   return (
-    <div className='bg-yellow-500 h-screen w-full flex flex-col justify-center items-center px-1 md:flex-row'>
+    <div className='bg-yellow-500 min-h-screen w-full flex flex-col justify-center items-center px-1 md:flex-row'>
       <div className='h-full w-full border-2 border-r-dotted border-red-400 flex justify-center items-center md:w-1/2'>
         <div className='flex flex-col justify-center items-center p-4 space-y-2'>
           <button onClick={()=>{setPresentation('TS')}} className='justify-center items-center text-xl text-gray-800 font-bold bg-blue-600 rounded-md px-2 py-1 active:bg-blue-700 active:text-white max-w-[200px] w-full break-words'>Total Sells</button>
